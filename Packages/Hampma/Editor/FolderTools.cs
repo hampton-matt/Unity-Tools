217cddb035b00be74d6b0dcc6295b81eba0356e2
hampton-matt <54047092+hampton-matt@users.noreply.github.com>
2023-02-27 16:49:19 +0000

    Updates Needs fixes

--- Packages/Hampma/Editor/FolderTools.cs
+++ Packages/Hampma/Editor/FolderTools.cs
@@ -1,44 +1,49 @@
+#if UNITY_EDITOR
 using System.IO;
 using UnityEditor;
 using static UnityEngine.Application;
 using static UnityEditor.AssetDatabase;
 
-namespace Hampma.Tools
+namespace Hampma
 {
-    public static class FolderTools
+    namespace Tools
     {
-        // FIRST SETUP
-        [MenuItem("Hampma/Tools/Project Setup", false)]
-        public static void CreateDefaultFolders()
+        public static class FolderTools
         {
-            if(!EditorUtility.DisplayDialog("Setup New Project",
-                "Would you like to setup the default folders and files for a new project?",
-                "Yes", "Cancel")) return;
-            CreateDirectories("", "Editor", "Project", "Project/Features", "Project/Game");
-            CreateDirectories("Project/Game", "Scenes", "Scripts", "Data");
-            Refresh();
-        }
-        [MenuItem("Hampma/Tools/Project Setup", true)]
-        public static bool CheckCreateDefaultFolders()
-        {
-            return !(IsValidFolder("Assets/Project") && IsValidFolder("Assets/Project/Features") && IsValidFolder("Assets/Project/Game"));
-        }
-
-        [MenuItem("Hampma/Tools/New Feature")]
-        public static void CreateNewFeature()
-        {
-            CreateDirectories("Project/Features", "NewFeature");
-            CreateDirectories("Project/Features/NewFeature", "Scripts", "Objects", "Scenes", "Art", "UI");
-            Refresh();
-        }
-
-        public static void CreateDirectories(string root, params string[] dirs)
-        {
-            var fullpath = Path.Combine(dataPath, root);
-            foreach (var dir in dirs)
+            // FIRST SETUP
+            [MenuItem("Hampma/Tools/Project Setup", false)]
+            public static void CreateDefaultFolders()
             {
-                Directory.CreateDirectory(Path.Combine(fullpath, dir));
+                if(!EditorUtility.DisplayDialog("Setup New Project",
+                    "Would you like to setup the default folders and files for a new project?",
+                    "Yes", "Cancel")) return;
+                CreateDirectories("", "Editor", "Project", "Project/Features", "Project/Game");
+                CreateDirectories("Project/Game", "Scenes", "Scripts", "Data");
+                Refresh();
+            }
+            [MenuItem("Hampma/Tools/Project Setup", true)]
+            public static bool ValidateCreateDefaultFolders()
+            {
+                return !(IsValidFolder("Assets/Project") && IsValidFolder("Assets/Project/Features") && IsValidFolder("Assets/Project/Game"));
+            }
+
+            [MenuItem("Hampma/Tools/New Feature")]
+            public static void CreateNewFeature()
+            {
+                CreateDirectories("Project/Features", "NewFeature");
+                CreateDirectories("Project/Features/NewFeature", "Scripts", "Objects", "Scenes", "Art", "UI");
+                Refresh();
+            }
+
+            public static void CreateDirectories(string root, params string[] dirs)
+            {
+                var fullpath = Path.Combine(dataPath, root);
+                foreach (var dir in dirs)
+                {
+                    Directory.CreateDirectory(Path.Combine(fullpath, dir));
+                }
             }
         }
     }
-}
+}   
+#endif
---
+++ Packages/Hampma/Scripts/S_BootManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Hampma {
+    namespace Tools {
+    public class S_BootManager : MonoBehaviour
+    {
+        [SerializeField]
+        private List<Hampma.Game.Scene> _BootScenes;
+    }
+}
+}
